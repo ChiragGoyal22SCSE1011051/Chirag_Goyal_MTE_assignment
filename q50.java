1848450450b0a132674b034b60526b4890a070bc
Chirag Goyal <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 14:28:46 +0530

    Create q50.java

---
+++ q50.java
@@ -0,0 +1,7 @@
+• Equilibrium Index is an index where the sum of the elements on its left is equal to the sum of
+the elements on its right.
+
+Applications:
+1. Efficient solutions for array problems involving balancing partitions.
+2. Used in problems where finding a balance point is critical, like load balancing in
+systems.
