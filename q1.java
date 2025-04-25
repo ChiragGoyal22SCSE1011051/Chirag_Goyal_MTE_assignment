ade64fac27b8b95c8176fcc7ec760757396a798b
ChiragGoyal22SCSE1011051 <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 11:57:03 +0530

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,6 @@
+A prefix sum array is an array where each element at index i
+contains the sum of all elements from the beginning up to index i of
+the original array.
+Example:
+arr = [2, 4, 1, 3, 5]
+prefix = [2, 6, 7, 10, 15]
