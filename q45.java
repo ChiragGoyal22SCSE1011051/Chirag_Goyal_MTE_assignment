e924f829863b14a0fe046024786d2ddf394413ad
Chirag Goyal <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 14:24:38 +0530

    Create q45.java

---
+++ q45.java
@@ -0,0 +1,12 @@
+• A priority queue is a data structure where each element is associated with a priority.
+• Elements with higher priorities are dequeued before those with lower priorities.
+
+Applications in algorithm design:
+1. Heap sort: A sorting algorithm using a priority queue (heap).
+2. Dijkstra's algorithm: For finding the shortest path in graphs.
+3. Huffman coding: For optimal encoding in data compression.
+4. Kth largest/smallest elements: Priority queues are used for efficiently finding the
+top K elements in an array.
+
+
+
