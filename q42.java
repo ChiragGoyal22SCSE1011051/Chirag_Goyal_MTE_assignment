612205ce16916e595d6ed1f2cc0b14a5fbbe529e
Chirag Goyal <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 14:22:10 +0530

    Create q42.java

---
+++ q42.java
@@ -0,0 +1,10 @@
+• Dynamic Programming (DP) is a method used to solve problems by breaking them down into
+simpler subproblems and solving each subproblem only once, storing the results to avoid
+redundant computations.
+  
+
+•In the maximum subarray problem, DP can be used to keep track of the maximum sum of
+the subarrays ending at each position in the array.
+  
+Kadane's algorithm is an example of a DP solution, where the maximum sum is computed iteratively while reusing the results of
+previous subproblems.
