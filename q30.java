768ddf0cd7a095fb90738eaca1fce91cb10b3ba9
ChiragGoyal22SCSE1011051 <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 12:47:57 +0530

    Create q30.java

---
+++ q30.java
@@ -0,0 +1,13 @@
+A linked list is a linear data structure where each element (node) contains data and a reference (or
+link) to the next node in the sequence.
+  
+Applications in algorithm design:
+
+• Efficient insertion/deletion:-> Insertion or deletion can be done in 0(1) time if the node is
+given.
+• Dynamic data structures:-> Linked lists can grow or shrink in size dynamically without
+reallocating or resizing.
+• Graph and tree representations:-> Used in adjacency lists, and can be used for sparse matrices
+in graphs
+
+
