f3db2ad7e21291625836b0e6532eb07f97a575f4
ChiragGoyal22SCSE1011051 <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 12:10:30 +0530

    Create q7.java

---
+++ q7.java
@@ -0,0 +1,9 @@
+The sliding window technique involves creating a window which can either be fixed or dynamic in size and moving it over the input to solve problems efficiently. 
+  It helps in reducing the time complexity of problems involving arrays or strings.
+  In string problems, 
+  it's used for:
+  
+•	Finding the longest/shortest substring with certain properties
+• Detecting anagrams
+•	Pattern matching
+• Optimizing space/time over brute-force solutions
