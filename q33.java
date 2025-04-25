f8e58b8624a568c55cb3b3291be2df8eb46a8556
ChiragGoyal22SCSE1011051 <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 12:51:19 +0530

    Create q33.java

---
+++ q33.java
@@ -0,0 +1,11 @@
+The sliding window technique involves maintaining a subset of elements in a sequence, usually in a
+contiguous block. The window l'slides" over the array, expanding or contracting as needed.
+  
+Applications:
+1.Maximum/Minimum in a sliding window
+2.Longest substring without repeating characters
+3.Subarray sum or product problems
+4.String matching problems
+
+For example,
+  finding the maximum sum of a subarray of size k can be solved by maintaining a window of size k and moving it across the array.
