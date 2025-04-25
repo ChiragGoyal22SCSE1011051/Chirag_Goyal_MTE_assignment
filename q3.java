3bad26e1a0f448b687ef3c973f9d5a45f6b8ce0b
ChiragGoyal22SCSE1011051 <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 12:04:58 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -1,34 +1,27 @@
+Algorithm:
+1.	Calculate total sum of array.
+2.	Iterate through array maintaining left_sum.
+3.	At each index, if left_sum == total - left_sum - arr[i],
+    return index.
+    
+Program:
 public class EquilibriumIndex {
-
     public static int findEquilibriumIndex(int[] arr) {
-        int totalSum = 0;
-        for (int num : arr) {
-            totalSum += num;
-        }
-
+        int total = 0;
+        for (int num : arr) total += num;
         int leftSum = 0;
         for (int i = 0; i < arr.length; i++) {
-            int rightSum = totalSum - leftSum - arr[i];
-            if (leftSum == rightSum) {
-                return i;
-            }
+            if (leftSum == total - leftSum - arr[i]) return i;
             leftSum += arr[i];
         }
-
         return -1;
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 2, 2};
-        int index = findEquilibriumIndex(arr);
-
-        if (index != -1) {
-            System.out.println("Equilibrium Index: " + index);
-        } else {
-            System.out.println("No Equilibrium Index Found");
-        }
+        System.out.println(findEquilibriumIndex(arr));
     }
 }
-
 Time Complexity: O(n)
 Space Complexity: O(1)
+Example: For array [1, 3, 5, 2, 2], index 2 is equilibrium index
