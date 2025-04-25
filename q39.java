f3a219cf27a75cfbee85a4db68c6008037b2fa6d
ChiragGoyal22SCSE1011051 <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 12:56:04 +0530

    Create q39.java

---
+++ q39.java
@@ -0,0 +1,6 @@
+• Subsets: A subset is any selection of elements from a set, without considering the order of elements. 
+  Example: For {I, 2}, the subsets are [J, [I], [2], [I, 2].
+
+  
+• Permutations: A permutation is an ordered arrangement of elements. 
+  Example: For {1, 2}, the permutations are [1, 2] and [2, 1].
