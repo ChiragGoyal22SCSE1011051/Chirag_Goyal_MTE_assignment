a750f4bdca2ea7eeb960f2448b89a681e76c42ef
ChiragGoyal22SCSE1011051 <chirag.22scse1011051@galgotiasuniversity.edu.in>
2025-04-25 12:30:21 +0530

    Create q24.java

---
+++ q24.java
@@ -0,0 +1,8 @@
+Explanation:
+Bit manipulation refers to the process of directly manipulating bits in a number using bitwise
+operators. It allows for efficient computation, especially for problems involving numbers and sets.
+  
+Key advantages include:
+• Faster execution: Bit operations are computationally inexpensive
+• Space optimization: Storing large sets or numbers using fewer bits.
+• Efficient algorithms: Used in dynamic programming, graph algorithms, and finding subsets.
